102cc2339917f99331b632c3533dd8491d964f85
ahmspd <ahmspdi@gmail.com>
2022-03-21 18:36:45 +0700

    update init table sql

--- linovcommunity/init_table.sql
+++ linovcommunity/init_table.sql
@@ -87,13 +87,42 @@ create table t_file(
 	is_active boolean default true
 );
 alter table t_file add constraint file_pk primary key(id);
+
+create table t_payment(
+	id uuid,
+	code varchar(10),
+	payment_name varchar(30),
+	created_by int,
+	created_at timestamp without time zone,
+	updated_by int,
+	updated_at timestamp without time zone,
+	"version" int,
+	is_active boolean default true
+);
+alter table t_payment add constraint payment_pk primary key(id);
+alter table t_payment add constraint payment_code_bk unique(code);
+
+create table t_price_list(
+	id uuid,
+	code varchar(10),
+	price_name varchar(30),
+	price int,
+	created_by int,
+	created_at timestamp without time zone,
+	updated_by int,
+	updated_at timestamp without time zone,
+	"version" int,
+	is_active boolean default true
+);
+alter table t_price_list add constraint price_list_pk primary key(id);
+alter table t_price_list add constraint price_list_code_bk unique(code);
 -- end master data
 
 create table t_user(
 	id uuid,
 	email varchar(30),
 	"password" varchar(255),
-	id_role int,
+	id_role uuid,
 	created_by int,
 	created_at timestamp without time zone,
 	updated_by int,
@@ -107,17 +136,18 @@ alter table t_user add constraint user_role_fk foreign key(id_role) references t
 
 create table t_profile(
 	id uuid,
-	id_user int,
+	id_user uuid,
 	full_name varchar(50),
 	phone_number varchar(15),
 	postal_code varchar(10),
 	company varchar(30),
-	id_industry int,
-	id_position int,
+	id_industry uuid,
+	id_position uuid,
+	is_member boolean default false,
 	instagram varchar(50),
 	twitter varchar(50),
 	facevook varchar(50),
-	id_file int,
+	id_file uuid,
 	created_by int,
 	created_at timestamp without time zone,
 	updated_by int,
@@ -131,12 +161,13 @@ alter table t_profile add constraint profile_industry_fk foreign key(id_industry
 alter table t_profile add constraint profile_position_fk foreign key(id_position) references t_position(id);
 alter table t_profile add constraint profile_file_fk foreign key(id_file) references t_file(id);
 
-create table t_thread(
+create table t_user_member(
 	id uuid,
-	title varchar(100),
-	contents bytea,
-	id_file int,
-	id_thread_type int,
+	id_user uuid,
+	id_payment uuid,
+	id_file uuid,
+	id_price_list uuid,
+	date_end timestamp,
 	created_by int,
 	created_at timestamp without time zone,
 	updated_by int,
@@ -144,13 +175,33 @@ create table t_thread(
 	"version" int,
 	is_active boolean default true
 );
-alter table t_threads add constraint t_threads_pk primary key(id);
-alter table t_threads add constraint t_threads_file_fk foreign key(id_file) references t_file(id);
-alter table t_threads add constraint t_threads_type_fk foreign key(id_thread_type) references t_thread_type(id);
+alter table t_user_member add constraint t_user_member_pk primary key(id);
+alter table t_user_member add constraint t_user_member_profile_fk foreign key(id_user) references t_user(id);
+alter table t_user_member add constraint t_user_member_payment_fk foreign key(id_payment) references t_payment(id);
+alter table t_user_member add constraint t_user_member_file_fk foreign key(id_file) references t_file(id);
+alter table t_user_member add constraint t_user_member_price_list_fk foreign key(id_price_list) references t_price_list(id);
+
+create table t_thread(
+	id uuid,
+	title varchar(100),
+	contents bytea,
+	id_file uuid,
+	id_thread_type uuid,
+	is_premium boolean default false,
+	created_by int,
+	created_at timestamp without time zone,
+	updated_by int,
+	updated_at timestamp without time zone,
+	"version" int,
+	is_active boolean default true
+);
+alter table t_thread add constraint t_threads_pk primary key(id);
+alter table t_thread add constraint t_threads_file_fk foreign key(id_file) references t_file(id);
+alter table t_thread add constraint t_threads_type_fk foreign key(id_thread_type) references t_thread_type(id);
 
 create table t_polling(
 	id uuid,
-	id_thread int,
+	id_thread uuid,
 	polling_name varchar(50),
 	created_by int,
 	created_at timestamp without time zone,
@@ -164,7 +215,7 @@ alter table t_polling add constraint t_polling_thread_fk foreign key(id_thread) 
 
 create table t_polling_detail(
 	id uuid,
-	id_polling int,
+	id_polling uuid,
 	polling_name varchar(50),
 	created_by int,
 	created_at timestamp without time zone,
@@ -178,7 +229,7 @@ alter table t_polling_detail add constraint t_polling_detail_polling_fk foreign 
 
 create table t_bookmark(
 	id uuid,
-	id_thread int,
+	id_thread uuid,
 	created_by int,
 	created_at timestamp without time zone,
 	updated_by int,
@@ -191,7 +242,7 @@ alter table t_bookmark add constraint t_bookmark_thread_fk foreign key(id_thread
 
 create table t_like(
 	id uuid,
-	id_thread int,
+	id_thread uuid,
 	created_by int,
 	created_at timestamp without time zone,
 	updated_by int,
@@ -200,5 +251,78 @@ create table t_like(
 	is_active boolean default true
 );
 alter table t_like add constraint t_like_pk primary key(id);
-alter table t_link add constraint t_like_thread_fk foreign key(id_thread) references t_thread(id);
+alter table t_like add constraint t_like_thread_fk foreign key(id_thread) references t_thread(id);
 
+create table t_event(
+	id uuid,
+	title varchar(35),
+	event_location varchar(50),
+	price int,
+	date_start timestamp,
+	date_end timestamp,
+	time_start time,
+	time_end time,
+	created_by int,
+	created_at timestamp without time zone,
+	updated_by int,
+	updated_at timestamp without time zone,
+	"version" int,
+	is_active boolean default true
+);
+alter table t_event add constraint event_pk primary key(id);
+
+create table t_event_payment(
+	id uuid,
+	id_event uuid,
+	id_payment uuid,
+	is_accept boolean default false,
+	id_file uuid,
+	created_by int,
+	created_at timestamp without time zone,
+	updated_by int,
+	updated_at timestamp without time zone,
+	"version" int,
+	is_active boolean default true
+);
+alter table t_event_payment add constraint event_payment_pk primary key(id);
+alter table t_event_payment add constraint event_payment_event_fk foreign key(id_event) references t_event(id);
+alter table t_event_payment add constraint event_payment_fk foreign key(id_payment) references t_payment(id);
+alter table t_event_payment add constraint event_payment_file_fk foreign key(id_file) references t_file(id);
+
+create table t_order_event(
+	id uuid,
+	id_event uuid,
+	id_user uuid,
+	is_accept boolean default false,
+	id_file uuid,
+	id_payment uuid,
+	created_by int,
+	created_at timestamp without time zone,
+	updated_by int,
+	updated_at timestamp without time zone,
+	"version" int,
+	is_active boolean default true
+);
+alter table t_order_event add constraint t_order_event_pk primary key(id);
+alter table t_order_event add constraint t_order_event_detail_fk foreign key(id_event) references t_event(id);
+alter table t_order_event add constraint t_order_event_user foreign key(id_user) references t_user(id);
+alter table t_order_event add constraint t_order_event_file foreign key(id_file) references t_file(id);
+alter table t_order_event add constraint t_order_event_payment foreign key(id_payment) references t_payment(id);
+
+--create table t_order_event_payment(
+--	id uuid,
+--	id_order_event int,
+--	is_accept boolean default false,
+--	id_file int,
+--	id_payment int,
+--	created_by int,
+--	created_at timestamp without time zone,
+--	updated_by int,
+--	updated_at timestamp without time zone,
+--	"version" int,
+--	is_active boolean default true
+--);
+--alter table t_order_event_payment add constraint t_order_event_payment_pk primary key(id);
+--alter table t_order_event_payment add constraint t_order_event_payemnt_detail_fk foreign key(id_order_event) references t_order_event(id);
+--alter table t_order_event_payment add constraint t_order_event_payment_file_fk foreign key(id_file) references t_file(id);
+--alter table t_order_event_payment add constraint t_order_event_payment_id foreign key(id_payment) references t_payment(id);
